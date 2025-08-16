548763c1d5fbfb1388f92932a156d8f0758a76cf
user.name : "Jay Kishan <user.email:"jkn18869!@gmail.com>
2025-08-16 23:50:47 +0530

    socket.io implement

--- Day_28-reStart-gpt-project/server.js
+++ Day_28-reStart-gpt-project/server.js
@@ -2,13 +2,13 @@
 const app = require('./src/app');
 const express = require('express')
 const connectTodb= require('./src/DB/db')
+const initSocketServer = require('./src/sockets/server.socket');
 
+const httpServer = require('http').createServer(app);
 
 connectTodb();
+initSocketServer(httpServer);
 
-
-
-
-app.listen(3000,()=>{
+httpServer.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
---
+++ Day_28-reStart-gpt-project/src/sockets/server.socket.js
@@ -0,0 +1,19 @@
+
+
+
+const { Server } = require("socket.io");
+
+
+function initSocketServer(httpServer){
+
+     const io = new Server(httpServer, {});
+
+     io.on("connection", (socket) => {
+         
+        console.log("New Socktet Connection ",socket.id);
+
+     })
+
+}
+
+module.exports = initSocketServer
