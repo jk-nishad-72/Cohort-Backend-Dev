5d1c32dd9127897042a92cf8fa311902ae227318
user.name : "Jay Kishan <user.email:"jkn18869!@gmail.com>
2025-08-20 22:28:25 +0530

    socket middleware used and implemented

--- Day_29-stm/src/socket/server.socket.js
+++ Day_29-stm/src/socket/server.socket.js
@@ -1,11 +1,48 @@
 
 const {Server} = require('socket.io')
+const cookie = require('cookie')
+const jwt = require('jsonwebtoken')
+const authModel = require('../models/auth.model');
+
 
 
 async function socketServer(httpServer) {
     
     
 const io = new Server(httpServer, { /* options */ });
+
+
+
+io.use(async (socket,next)=>{
+
+     const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+    //   const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+     console.log(cookies.token)
+
+     if(!cookies.token){
+
+        next(new Error("Authentication error : No token provided"));
+     }
+
+     try{
+
+        const decode = jwt.verify(cookies.token,process.env.JWT_SECRET_KEY)
+
+         const user = await authModel.findById(decode.id)
+
+         socket.user = user
+         next()
+         
+     }
+     catch(error){
+
+        next(new Error("Authentication error:Invalid error"))
+
+     }
+
+
+
+})
 
 io.on("connection", (socket) => {
   
